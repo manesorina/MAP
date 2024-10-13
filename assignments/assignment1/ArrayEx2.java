aa82264e965eb58c369c1a3be08850e3bdc368e5
so17 <manesorina@gmail.com>
2024-10-13 23:25:08 +0300

    Finished ex3

--- assignments/assignment1/ArrayEx2.java
+++ assignments/assignment1/ArrayEx2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ArrayEx2 {
     int[] arr1;
     int[] arr2;
@@ -6,6 +8,113 @@ public class ArrayEx2 {
         this.arr1=arr1;
         this.arr2=arr2;
     }
+
+    public int[] calculateSum(int[] arr1, int[] arr2){
+        int i=arr1.length-1;
+        int j=arr2.length-1;
+        int[] sumArray=new int[arr1.length];
+        int k=sumArray.length-1;
+
+        int carry=0;
+
+        while(i>=0){
+            int sum = arr1[i] + arr2[j] + carry;
+            if (sum >= 10) {
+                    carry = sum / 10;
+                    sum = sum % 10;
+                } else {
+                    carry = 0;
+                }
+
+                sumArray[k]=sum;
+                i--;
+                j--;
+                k--;
+            }
+
+        if (carry>0){
+            sumArray[0]=carry;
+        }
+
+
+        return sumArray;
+
+
+    }
+
+    public int[] calculateDifference(int[] arr1, int[] arr2) {
+        int[] diffArray = new int[arr1.length];
+        int i = arr1.length - 1;
+        int j = arr2.length - 1;
+        int k = diffArray.length - 1;
+
+
+        while (i >= 0) {
+            if (arr1[i] < arr2[j]) {
+
+                if (i > 0) {
+                    arr1[i - 1]--;
+                    arr1[i] += 10;
+                }
+            }
+            diffArray[k] = arr1[i] - arr2[j] ;
+            k = k - 1;
+            i = i - 1;
+            j = j - 1;
+        }
+
+        return diffArray;
+    }
+
+    public int[] multiply(int[]arr1, int nr) {
+        int[] multiplyArr=new int[arr1.length];
+        int i=arr1.length-1;
+        int k=multiplyArr.length-1;
+        int carry=0;
+        while(i>=0){
+            int prod=arr1[i]*nr+carry;
+            if(prod>10){
+                carry=prod/10;
+                prod=prod%10;
+            }else{
+                carry=0;
+            }
+            multiplyArr[k]=prod;
+            i--;
+            k--;
+
+        }
+        if(carry>0){
+            multiplyArr[0]+=carry;
+        }
+        return multiplyArr;
+    }
+
+    public int[] divide(int[] arr1, int divisor) {
+        if (divisor <= 0 ) {
+            throw new IllegalArgumentException("Divisor must be greater than 0");
+        }
+
+        int[] quotient = new int[arr1.length];
+        int rest = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            int current = rest * 10 + arr1[i];
+            quotient[i] = current / divisor;
+            rest = current % divisor;
+        }
+
+        int startIndex = 0;
+        while (startIndex < quotient.length - 1 && quotient[startIndex] == 0) {
+            startIndex++;
+        }
+
+        int[] result = new int[quotient.length - startIndex];
+        System.arraycopy(quotient, startIndex, result, 0, result.length);
+        return result;
+    }
+
+
 
 
 }
--- assignments/assignment1/Main.java
+++ assignments/assignment1/Main.java
@@ -28,6 +28,36 @@ public class Main{
         System.out.print("The max sum of n-1 numbers is: "+arrayObject.maxSum()+"\n");
         System.out.print("The min sum of n-1 numbers is: "+arrayObject.minSum()+"\n");
 
+        System.out.print("Ex3"+"\n");
+        int[] arr1={1,3,0,0,0,0,0,0,0};
+        int[] arr2={8,7,0,0,0,0,0,0,0};
+
+
+        ArrayEx2 arrayEx2=new ArrayEx2(arr1,arr2);
+        System.out.print("The sum is: "+"\n");
+        for (int num:arrayEx2.calculateSum(arr1,arr2)){
+            System.out.print(num+ " ");
+        }
+
+        System.out.print("\n"+"The difference is: "+"\n");
+
+        int[] arr3={8,3,0, 0, 0, 0, 0, 0, 0};
+        int[] arr4={5,4, 0, 0, 0, 0, 0, 0, 0};
+        for(int num:arrayEx2.calculateDifference(arr3,arr4)){
+            System.out.print(num+" ");
+        }
+
+        System.out.print("\n"+"The product is: "+"\n");
+        int[] arr5={2,3,6,0,0,0,0,0,0};
+        for(int num:arrayEx2.multiply(arr5,2)){
+            System.out.print(num+" ");
+        }
+
+        System.out.print("\n"+"The dvision is: "+"\n");
+        int[] arr6={2,3,6,0,0,0,0,0,0};
+        for(int num:arrayEx2.divide(arr6,2)) {
+            System.out.print(num+" ");
+        }
 
 
     }
